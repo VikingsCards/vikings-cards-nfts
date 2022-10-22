17186ee9c6e5bf8ebcadf5902d55a697bebcf372
Polarys Fundation <116130129+VikingsCards@users.noreply.github.com>
2022-10-22 16:08:24 -0400

    Community Group Updated

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Footer.css'
 import '../css/media.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
 
 
 const Footer = () => {
@@ -8,14 +13,18 @@ const Footer = () => {
     <div>
       <div className="footer-box">
         <div className="footer-box2">
-            <div className="footer-box3">
-                <div className="footer-box4">
-                    <div className="footer-box5">
-                        <p className="footer-p media-footer-p">©2022, Vikings Cards NFTs</p>
-                        <p className="footer-p media-footer-p">To conquer the sea of ​​solana</p>
-                    </div>
+          <div className="footer-box3">
+            <div className="footer-box4">
+              <div className="footer-box5">
+                <p className="footer-p media-footer-p">©2022, Vikings Cards NFTs</p>
+                <div className="footer-box6">
+                  <a rel="noreferrer" target="_blank" href="https://discord.gg/aAT9cVUj5X" className="footer-a"><FontAwesomeIcon className='icon-community' icon={faDiscord} /></a>
+                  <a rel="noreferrer" target="_blank" href="https://twitter.com/Vikings_Guild" className="footer-a"><FontAwesomeIcon className='icon-community' icon={faTwitter} /></a>
                 </div>
+                <p className="footer-p media-footer-p">To conquer the sea of ​​solana</p>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
